003f1d7126a0a52166f68510072eeb428dc9c45e
reagan02 <r.mabawad.529147@umindanao.edu.ph>
2024-05-18 22:52:17 +0800

    remove vercel file

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,7 @@ const app = express();
 const cors = require("cors");
 app.use(express.json());
 app.use(cors(
-  {
-    origin: ["http://localhost:3000"],
-    credentials: true,
-  }
+  
 ));
 
 // express session
