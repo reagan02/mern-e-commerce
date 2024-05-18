35d74fd300bbc7da1b4bfb245679aec31e38756f
reagan02 <r.mabawad.529147@umindanao.edu.ph>
2024-05-19 00:18:16 +0800

    update images

--- frontend/src/Pages/Homepage.jsx
+++ frontend/src/Pages/Homepage.jsx
@@ -4,6 +4,10 @@ import Section1 from "../Components/Homepage/Section1";
 import Category from "../Components/Homepage/Category";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import PS4 from "../src/assests/PS4.png";
+import Women from "../src/assests/Women.png";
+import Speaker from "../src/assests/Speaker.png";
+import Perfume from "../src/assests/Perfume.png";
 import "react-multi-carousel/lib/styles.css";
 
 import {
@@ -78,17 +82,17 @@ const Homepage = () => {
 				title="New Arrival"
 				body={
 					<div className="grid xs:grid-cols-2 xs:grid-rows-2  md:grid-cols-4 md:grid-rows-2 xs:gap-5 md::gap-8 lg:gap-10">
-						<div className=" md:col-span-2 md:row-span-2">
-							<img src="../src/assests/PS4.png " alt="" />
+						<div className="md:col-span-2 md:row-span-2">
+							<img src={PS4} alt="" />
 						</div>
-						<div className="md:col-span-2 ">
-							<img src="../src/assests/Women.png" alt="" />
-						</div>
-						<div className="">
-							<img src="../src/assests/Speaker.png" alt="" />
+						<div className="md:col-span-2">
+							<img src={Women} alt="" />
 						</div>
 						<div className="">
-							<img src="../src/assests/Perfume.png" alt="" />
+							<img src={Speaker} alt="" />
+						</div>
+						<div className="">
+							<img src={Perfume} alt="" />
 						</div>
 					</div>
 				}
