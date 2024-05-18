f8953b59b1598ee5ebd4f02e9be3cdc6d1c251c8
reagan02 <r.mabawad.529147@umindanao.edu.ph>
2024-05-19 02:13:08 +0800

    fix CORS issue

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 const cors = require("cors");
 app.use(express.json());
 app.use(cors({
-  origin: "https://exclusive-api.vercel.app/", 
+  origin: "https://deploy-ecommerce-frontend.vercel.app", 
   methods: ['GET', 'POST'], 
   credentials: true,
 }));
